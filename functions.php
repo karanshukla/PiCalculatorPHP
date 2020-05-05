ac3ffb474e17d9d0308d74ee305c913bac4ec70b
Karan Shukla <karan.shukla@loopio.com>
2020-05-05 15:17:31 -0400

    Split into Functions
    Split file up into Functions library and index file

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+	function bcpi($precision){
+
+    $num = 0;$k = 0;
+    bcscale($precision+3);
+
+    $limit = ($precision+3)/14;
+    while($k < $limit){
+        $num = bcadd($num,bcdiv(bcmul(bcadd('13591409',bcmul('545140134', $k)),bcmul(bcpow(-1, $k), bcfact(6*$k))),bcmul(bcmul(bcpow('640320',3*$k+1),bcsqrt('640320')), bcmul(bcfact(3*$k), bcpow(bcfact($k),3)))));
+    ++$k;
+}
+	
+	return bcdiv(1,(bcmul(12,($num))),$precision);
+}
+	function bcfact($n){
+  	return ($n == 0 || $n== 1) ? 1 : bcmul($n,bcfact($n-1));
+}
+?>
